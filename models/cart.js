82a0b6f339e857c2a8a3081b1791f265abd7b75e
Gunawan Nur Ahmad <54525132+Gunawanart@users.noreply.github.com>
2020-06-19 19:55:29 +0700

    finish cart delete

--- models/cart.js
+++ models/cart.js
@@ -41,6 +41,9 @@ module.exports = class Cart {
             }
             const updatedCart = {...JSON.parse(fileContent)} 
             const product = updatedCart.products.find(prod => prod.id === id)
+            if (!product) {
+                return;
+            }
             updatedCart.products =  updatedCart.products.filter(prod => prod.id !== id)
             updatedCart.totalPrice = updatedCart.totalPrice - price * product.qty
             console.log(updatedCart.totalPrice - price * product.qty)
